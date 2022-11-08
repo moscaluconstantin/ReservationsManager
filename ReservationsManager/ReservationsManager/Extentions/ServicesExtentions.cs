75c6809c216c1ba53645f52da5a1b822eb3471f9
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-11-08 10:49:11 +0200

    Add ServiceExtentions.cs

---
+++ ReservationsManager/ReservationsManager/Extentions/ServicesExtentions.cs
@@ -0,0 +1,65 @@
+﻿using EFCoreMappingApp;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ReservationsManager.BLL.Interfaces;
+using ReservationsManager.BLL.Services;
+using ReservationsManager.DAL.Interfaces;
+using ReservationsManager.DAL.Repositories;
+using System.Text;
+
+namespace ReservationsManager.API.Extentions
+{
+    public static class ServicesExtentions
+    {
+        public static void AddDbContexts(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            string connectionString = configuration.GetConnectionString("myDb1");
+            services.AddSingleton(x => new RezervationsDbContext(connectionString));
+
+            // For Entity Framework
+            services.AddDbContext<RezervationsDbContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IActionsRepository, ActionsRepository>();
+            services.AddScoped<IReservationsRepository, ReservationsRepository>();
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped<IEmployeesRepository, EmployeesRepository>();
+            services.AddScoped<IActionEmployeesRepository, ActionEmployeesRepository>();
+            services.AddScoped<ITimeBlocksRepository, TimeBlocksRepository>();
+            services.AddScoped<IUserCredentialsRepository, UserCredentialsRepository>();
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUsersService, UsersService>();
+            services.AddScoped<IEmployeesService, EmployeesService>();
+            services.AddScoped<IActionEmployeesService, ActionEmployeesService>();
+            services.AddScoped<IReservationsService, ReservationsService>();
+            services.AddScoped<IJwtTokenService, JwtTokenService>();
+        }
+
+        public static void AddJwtAuthentication(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            var jwtSecretKey = configuration.GetValue<string>("Jwt:SecretKey");
+            services.AddAuthentication(item =>
+            {
+                item.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                item.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(item =>
+            {
+                item.RequireHttpsMetadata = true;
+                item.SaveToken = true;
+                item.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+    }
+}
--- ReservationsManager/ReservationsManager/Program.cs
+++ ReservationsManager/ReservationsManager/Program.cs
@@ -1,64 +1,20 @@
 using EFCoreMappingApp;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
+using ReservationsManager.API.Extentions;
 using ReservationsManager.BLL;
-using ReservationsManager.BLL.Interfaces;
-using ReservationsManager.BLL.Services;
-using ReservationsManager.DAL.Interfaces;
-using ReservationsManager.DAL.Repositories;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string connectionString = builder.Configuration.GetConnectionString("myDb1");
-builder.Services.AddSingleton(x => new RezervationsDbContext(connectionString));
-
-//Repositories
-builder.Services.AddScoped<IActionsRepository, ActionsRepository>();
-builder.Services.AddScoped<IReservationsRepository, ReservationsRepository>();
-builder.Services.AddScoped<IUsersRepository, UsersRepository>();
-builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
-builder.Services.AddScoped<IActionEmployeesRepository, ActionEmployeesRepository>();
-builder.Services.AddScoped<ITimeBlocksRepository, TimeBlocksRepository>();
-builder.Services.AddScoped<IUserCredentialsRepository, UserCredentialsRepository>();
-
-//Services
-builder.Services.AddScoped<IUsersService, UsersService>();
-builder.Services.AddScoped<IEmployeesService, EmployeesService>();
-builder.Services.AddScoped<IActionEmployeesService, ActionEmployeesService>();
-builder.Services.AddScoped<IReservationsService, ReservationsService>();
-builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
-
-// For Entity Framework
-builder.Services.AddDbContext<RezervationsDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContexts(builder.Configuration);
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 // For Identity
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<RezervationsDbContext>()
     .AddDefaultTokenProviders();
-
-
-var jwtSecretKey = builder.Configuration.GetValue<string>("Jwt:SecretKey");
-builder.Services.AddAuthentication(item =>
-{
-    item.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    item.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(item =>
-{
-    item.RequireHttpsMetadata = true;
-    item.SaveToken = true;
-    item.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretKey)),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
 
 builder.Services.AddAutoMapper(typeof(BllAssemblyMarker));
 builder.Services.AddControllers();

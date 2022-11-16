98669341b19475fdbb3c665080eb143bdb931d36
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-11-16 12:57:02 +0200

    Add UserAccountComponent.

---
+++ ReservationsManager/ReservationsManager.Angular/src/app/modules/user/user-account/user-account.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserAccountComponent } from './user-account.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserAccountComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserAccountModule { }

855f9230cefd7c7205c212c586bec2faf862f81b
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-11-24 21:24:17 +0200

    Add user cancel logic.

--- ReservationsManager/ReservationsManager.Angular/src/app/modules/user/user-reservations-list/user-reservations-list.component.ts
+++ ReservationsManager/ReservationsManager.Angular/src/app/modules/user/user-reservations-list/user-reservations-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { finalize } from 'rxjs';
+import { ReservationCanceledUpdateDto } from 'src/app/_models/Reservation/ReservationCanceledUpdateDto';
 import { UserReservationDto } from 'src/app/_models/Reservation/UserReservationDto';
 import { ReservationsService } from 'src/app/_services/Reservations/reservations.service';
 
@@ -8,8 +11,11 @@ import { ReservationsService } from 'src/app/_services/Reservations/reservations
   styleUrls: ['./user-reservations-list.component.css'],
 })
 export class UserReservationsListComponent implements OnInit {
+  @ViewChild('myTable') table: MatTable<any> | undefined;
   reservations: Array<UserReservationDto>;
-  columnsToDisplay = ['employee', 'action', 'date', 'time'];
+  columnsToDisplay = ['employee', 'action', 'date', 'time', 'status', 'button'];
+
+  private inProcess: boolean = false;
 
   constructor(private reservationsService: ReservationsService) {
     this.reservations = new Array<UserReservationDto>();
@@ -23,5 +29,29 @@ export class UserReservationsListComponent implements OnInit {
     this.reservationsService
       .getUserReservations()
       .subscribe((reservations) => (this.reservations = reservations));
+  }
+
+  update(reservation: UserReservationDto): void {
+    if (this.inProcess) return;
+
+    this.inProcess = true;
+    let updateDto = new ReservationCanceledUpdateDto(
+      reservation.id,
+      !reservation.canceled
+    );
+
+    this.reservationsService
+      .updateCanceledState(updateDto)
+      .pipe(finalize(() => (this.inProcess = false)))
+      .subscribe(() => this.removeReservation(reservation));
+  }
+
+  private removeReservation(reservation: UserReservationDto): void {
+    const index = this.reservations.indexOf(reservation, 0);
+
+    if (index >= 0) {
+      this.reservations.splice(index, 1);
+      this.table?.renderRows();
+    }
   }
 }

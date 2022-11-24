e4b8e50615b5d5fda9b9d407f74b0234fbbaf498
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-11-24 21:28:36 +0200

    Update employee-reservations-list.component.ts

--- ReservationsManager/ReservationsManager.Angular/src/app/modules/employee/employee-reservations-list/employee-reservations-list.component.ts
+++ ReservationsManager/ReservationsManager.Angular/src/app/modules/employee/employee-reservations-list/employee-reservations-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs';
 import { EmployeeReservationDto } from 'src/app/_models/Reservation/EmployeeReservationDto';
 import { ReservationCanceledUpdateDto } from 'src/app/_models/Reservation/ReservationCanceledUpdateDto';
 import { ReservationsService } from 'src/app/_services/Reservations/reservations.service';
@@ -11,6 +12,8 @@ import { ReservationsService } from 'src/app/_services/Reservations/reservations
 export class EmployeeReservationsListComponent implements OnInit {
   reservations: Array<EmployeeReservationDto>;
   columnsToDisplay = ['user', 'action', 'date', 'time', 'status', 'button'];
+
+  private inProcess: boolean = false;
 
   constructor(private reservationsService: ReservationsService) {
     this.reservations = new Array<EmployeeReservationDto>();
@@ -27,12 +30,18 @@ export class EmployeeReservationsListComponent implements OnInit {
   }
 
   update(reservation: EmployeeReservationDto): void {
+    if (this.inProcess) return;
+
+    this.inProcess = true;
     reservation.canceled = !reservation.canceled;
     let updateDto = new ReservationCanceledUpdateDto(
       reservation.id,
       reservation.canceled
     );
 
-    this.reservationsService.updateCanceledState(updateDto).subscribe();
+    this.reservationsService
+      .updateCanceledState(updateDto)
+      .pipe(finalize(() => (this.inProcess = false)))
+      .subscribe();
   }
 }
